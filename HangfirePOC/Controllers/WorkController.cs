f813e27a656f6443093f997e176c51a6fb75260d
Ahmad Arsyad Bin. Ahmad <arsh90@gmail.com>
2017-01-24 18:35:37 +0800

    initial commit

---
+++ HangfirePOC/Controllers/WorkController.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangfirePOC.Web.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult Index()
+        {
+            BackgroundJob.Enqueue(() => Console.Write("Lock dog")); 
+        }
+    }
+}
---
+++ HangfirePOC/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Hangfire;
+
+[assembly: OwinStartupAttribute(typeof(HangfirePOC.Startup))]
+namespace HangfirePOC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage("HangfireDB");
+
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
